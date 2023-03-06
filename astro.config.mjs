4c6145b04df44e3c3d51aa085f7bdef370a8ebd7
Morten André Lines <morten@jobbnorge.no>
2023-03-06 08:56:02 +0100

    Byttet ut preact med vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,2 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [preact()]
-});
+import vue from "@astrojs/vue";
